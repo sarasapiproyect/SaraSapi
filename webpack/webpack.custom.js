0d84721bc5ec8b15f5f050727ef33e2c3a18e15e
yalmea <yalmea@boldt.com.ar>
2023-10-04 21:52:41 -0300

    cambios de petete

--- webpack/webpack.custom.js
+++ webpack/webpack.custom.js
@@ -30,7 +30,7 @@ module.exports = async (config, options, targetOptions) => {
         },
       }),
       new WebpackNotifierPlugin({
-        title: 'Sara Business Inteligente',
+        title: 'Petete Business Inteligente',
         contentImage: path.join(__dirname, 'logo-jhipster.png'),
       })
     );
